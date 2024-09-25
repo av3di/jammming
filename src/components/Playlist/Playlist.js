16cf432c0395eb82369d406ff169d2d7be993ba9
av3di <e_uribe2011@yahoo.com>
2024-09-25 15:52:36 -0700

    more styling for playlist, added name state

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Playlist.css';
+import SaveToSpotifyButton from '../SaveToSpotifyButton/SaveToSpotifyButton';
 
 function Playlist() {
+  const [name, setName]  = useState('');
+  const [tracks, setTracks] = useState([]);
+
+  const handleChange = ({target}) => setName(target.value);
+
   return (
     <div className="panel">
       <h3>playlist</h3>
-      <input placeholder="Playlist Name" type="text" />
+      <input placeholder="Playlist Name"
+        type="text"
+        value={name}
+        onChange={handleChange}
+      />
+      <SaveToSpotifyButton />
     </div>
   );
 }
---
+++ src/components/SaveToSpotifyButton/SaveToSpotifyButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SaveToSpotifyButton() {
+  return (
+    <div>
+      <button>save playlist to spotify</button>
+    </div>
+  );
+}
+
+export default SaveToSpotifyButton;
