448894e062ea39df945697d4a2b0712008e0faf1
av3di <e_uribe2011@yahoo.com>
2024-09-26 16:10:09 -0700

    getting authorization from Spotify and user

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import React, {useState} from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import tracksResults from './tracks';
-import TrackList from './components/TrackList/TrackList';
 import Playlist from './components/Playlist/Playlist';
 import SearchResults from './components/SearchResults/SearchResults';
+import Spotify from './util/Spotify';
+let test = '';
+let code = '';
 
 function App() {
+
+  const params = new URLSearchParams(window.location.search);
+  if(params.get("error")) console.log('you need to give access');
+  if(params.get("code")) code = params.get("code");
+
+  const handleClick = async () => {
+    console.log('calling authorize');
+    if(!code) await Spotify.authorize();
+    console.log('after auth in app');
+    console.log(code);
+    let accessToken = '';
+    if (code) accessToken = await Spotify.getAccessToken(code);
+    console.log('accessToken:')
+    console.log(accessToken);
+    test = accessToken;
+  }
+
+  const handleAK = () => {
+    console.log('access::');
+    console.log(test);
+  }
   const [playlistName, setPlaylistName]  = useState('');
   const [playlistTracks, setPlaylistTracks] = useState([]);
 
@@ -21,9 +44,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>jammming</h1>
+        <h1 onClick={handleClick}>jammming</h1>
       </header>
       <SearchBar />
+      <button onClick={handleAK}>AK</button>
       <div className="main-panel">
         <SearchResults tracks={tracksResults} onAdd={addTrack} />
         <Playlist
---
+++ src/util/Spotify.js
@@ -0,0 +1,69 @@
+const clientId = '';
+const redirectUri = 'http://localhost:3000/';
+
+function generateCodeVerifier(length) {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+async function generateCodeChallenge(verifier) {
+  const data = new TextEncoder().encode(verifier);
+  const hashed = await window.crypto.subtle.digest('SHA-256', data);
+  return btoa(String.fromCharCode(...new Uint8Array(hashed)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+const Spotify = {
+  async authorize() {
+    console.log('generating code');
+    const codeVerifier = generateCodeVerifier(64);
+    const codeChallenge = await generateCodeChallenge(codeVerifier);
+    window.localStorage.setItem('code_verifier', codeVerifier);
+
+    const url = 'https://accounts.spotify.com/authorize';
+    const params = new URLSearchParams({
+      response_type: 'code',
+      client_id: clientId,
+      scope: 'playlist-modify-public',
+      code_challenge_method: 'S256',
+      code_challenge: codeChallenge,
+      redirect_uri: redirectUri,
+    });
+
+    window.location.href = `${url}?${params.toString()}`;
+    console.log('end of authorize');
+  },
+  async getAccessToken(code) {
+    console.log('getAccessToken');
+    let codeVerifier = window.localStorage.getItem('code_verifier');
+
+    const payload = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        client_id: clientId,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier,
+      }),
+    }
+
+    const url = 'https://accounts.spotify.com/api/token';
+    try {
+      const result = await fetch(url, payload);
+      const response = await result.json();
+      return response;
+    } catch (error) {
+      console.log("No access token created: " + error);
+      throw error;
+    }
+    // localStorage.setItem('access_token', response.access_token);
+  }
+}
+
+export default Spotify;
