20beabcae19f4341060a0a9afd3d0e741fb9fa1a
av3di <e_uribe2011@yahoo.com>
2024-09-29 18:56:37 -0700

    handle no results

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -3,9 +3,11 @@ import './SearchResults.css';
 import TrackList from '../TrackList/TrackList';
 
 function SearchResults(props) {
+  const noResults = <p className="no-results">no results</p>;
   return (
     <div className="panel">
       <h3>results</h3>
+      {props.tracks.length === 0 && noResults}
       <TrackList tracks={props.tracks} onAdd={props.onAdd} />
     </div>
   );
